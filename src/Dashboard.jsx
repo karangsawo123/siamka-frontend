1b2af55521e8974d65e9c0210791a71850b1a89f
Dicky <karangsawo123@gmail.com>
2025-10-04 16:19:14 +0700

    hari ke 4

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -2,7 +2,7 @@ function Dashboard({ user }) {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Selamat datang di Dashboard SIAMKA 🎓</h1>
-      <p>Halo, {user.name}</p>
+      <p>Halo {user.role?.nama_role}, Kampus</p>
     </div>
   );
 }
